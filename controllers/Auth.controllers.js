ee747b3a22e6d7096ed27f5973d32a221a321f59
Amr Hesham <msam.team20@gmail.com>
2024-04-17 16:07:43 +0200

    handle db error

--- controllers/Auth.controllers.js
+++ controllers/Auth.controllers.js
@@ -1,15 +1,14 @@
-const mongose = require('mongoose');
+const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const { genrateJwtToken, genrateJwtRefreshToken } = require('../utils/jwt');
 const user = require('../models/user');
 const resStatus = require('../utils/response.status');
 const Hash_Password = require('../utils/hash.password');
 
-
 async function Register(request, response) {
 	const { firstName, lastName, email, password } = request?.body;
 	try {
-		await mongose.connect(process.env.DATABASE_CONNECTION_STRING);
+		await mongoose.connect(process.env.DATABASE_CONNECTION_STRING);
 		const isUserExist = await user.findOne({ email });
 		if (!isUserExist) {
 			const createdUser = await user.create({
@@ -43,12 +42,14 @@ async function Register(request, response) {
 			data: {},
 			message: 'Error occured when try to connect to database.',
 		});
+	} finally {
+		mongoose.disconnect();
 	}
 }
 async function Login(request, response) {
 	const { email, password } = request?.body;
 	try {
-		await mongose.connect(process.env.DATABASE_CONNECTION_STRING);
+		await mongoose.connect(process.env.DATABASE_CONNECTION_STRING);
 		const isUserExist = await user.findOne({ email });
 
 		if (isUserExist) {
@@ -66,6 +67,8 @@ async function Login(request, response) {
 
 				response.cookie('RefreshToken', refToken, {
 					maxAge: 7 * 24 * 60 * 60 * 1000,
+					httpOnly: true,
+					secure: true,
 				});
 				return response.status(200).json({
 					status: resStatus.SUCCESS,
@@ -90,6 +93,13 @@ async function Login(request, response) {
 		}
 	} catch (error) {
 		console.log(error);
+		return response.status(500).json({
+			status: resStatus.ERROR,
+			data: {},
+			message: 'Error occured when try to connect to database.',
+		});
+	} finally {
+		mongoose.disconnect();
 	}
 }
 async function Logout(request, response) {
--- controllers/wathlist.controllers.js
+++ controllers/wathlist.controllers.js
@@ -1,4 +1,4 @@
-const { default: mongoose } = require('mongoose');
+const mongoose = require('mongoose');
 const watchlist = require('../models/watchlist');
 const resStatus = require('../utils/response.status');
 
